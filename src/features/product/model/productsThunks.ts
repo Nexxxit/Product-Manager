68020b03d710bf87f3159ac3917d1d06cbf097d0
Nexxxit <varsik.arsik@mail.ru>
2025-10-19 20:21:11 +0500

    feat: add product thunks

---
+++ src/features/product/model/productsThunks.ts
@@ -0,0 +1,11 @@
+import {createAsyncThunk} from "@reduxjs/toolkit";
+import {productsApi} from "./productsApi.ts";
+import type {NewProduct, Product} from "./types.ts";
+
+export const fetchProducts = createAsyncThunk<Product[]>('product/fetchAll', productsApi.getProducts)
+export const fetchCategories = createAsyncThunk<string[]>('product/fetchCategories', productsApi.getCategories)
+export const addProduct = createAsyncThunk<Product, NewProduct>('product/add', productsApi.addProduct)
+export const updateProduct = createAsyncThunk<Product, Partial<Product> & Pick<Product, 'id'>>(
+    'product/update', productsApi.updateProduct
+)
+export const deleteProduct = createAsyncThunk<number, number>('product/delete', productsApi.deleteProduct)
