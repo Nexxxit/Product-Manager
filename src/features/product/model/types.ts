936ae412ede7410fbebb6d59daa1ad186729118a
Nexxxit <varsik.arsik@mail.ru>
2025-10-19 18:30:10 +0500

    chore: add product types

---
+++ src/features/product/model/types.ts
@@ -0,0 +1,13 @@
+export type Rating = { rate: number, count: number }
+
+export type Product = {
+    id: number;
+    title: string;
+    price: number;
+    description: string;
+    category: string;
+    image: string;
+    rating: Rating;
+}
+
+export type NewProduct = Omit<Product, 'id | rating'> & {rating?: Rating}
