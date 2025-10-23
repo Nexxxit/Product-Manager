c3263eb2c538a0c712fdfefb3a08fb7eb2c2ae3e
Nexxxit <varsik.arsik@mail.ru>
2025-10-23 22:53:37 +0500

    feat: add mocks data import in dev mode

--- src/features/product/model/productsThunks.ts
+++ src/features/product/model/productsThunks.ts
@@ -4,7 +4,19 @@ import type {NewProduct, Product} from "./types.ts";
 
 export const fetchProducts = createAsyncThunk<Product[]>('product/fetchAll', productsApi.getProducts)
 export const fetchProductById = createAsyncThunk<Product, number>('product/fetchById', productsApi.getProduct)
-export const fetchCategories = createAsyncThunk<string[]>('product/fetchCategories', productsApi.getCategories)
+
+export const fetchCategories = createAsyncThunk<string[]>(
+    'product/fetchCategories',
+    async () => {
+        if (import.meta.env.DEV) {
+            const {mockCategories} = await import('./mocks.ts');
+            return mockCategories;
+        }
+        return productsApi.getCategories()
+    }
+    )
+
+
 export const addProduct = createAsyncThunk<Product, NewProduct>('product/add', productsApi.addProduct)
 export const updateProduct = createAsyncThunk<Product, Partial<Product> & Pick<Product, 'id'>>(
     'product/update', productsApi.updateProduct
