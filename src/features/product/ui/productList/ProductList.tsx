32f0d89cf9a6d143514a529ba06c95ce1c961acd
Nexxxit <varsik.arsik@mail.ru>
2025-10-25 18:00:09 +0500

    feat: add processing for cases when empty product list

--- src/features/product/ui/productList/ProductList.tsx
+++ src/features/product/ui/productList/ProductList.tsx
@@ -2,21 +2,21 @@ import {useAppSelector, useAppDispatch} from "../../../../app/store/hooks.ts";
 import {fetchProducts, selectSortedProducts, selectStatus} from "../../model";
 import {useEffect} from "react";
 import ProductCard from "../productCard/ProductCard.tsx";
-import {hydrate} from "../../model/slice.ts";
-import {mockProducts} from "../../model/mocks.ts";
 import Spinner from "../../../../shared/ui/Spinner/Spinner.tsx";
 import './productList.css'
 
-const ProductList = () => {
+type ProductListProps = {
+    onItemClick: (id: number) => void;
+}
+
+const ProductList = ({onItemClick}: ProductListProps) => {
     const dispatch = useAppDispatch()
     const status = useAppSelector(selectStatus)
     const products = useAppSelector(selectSortedProducts)
 
     useEffect(() => {
-        if(import.meta.env.DEV) {
-            dispatch(hydrate(mockProducts))
-        } else {
-            if(status === 'idle') dispatch(fetchProducts())
+        if (status === 'idle') {
+            dispatch(fetchProducts())
         }
     }, [dispatch, status]);
 
@@ -29,7 +29,9 @@ const ProductList = () => {
 
     return (
         <div className={'products-list'}>
-            {products.map(p => <ProductCard key={p.id} id={p.id}/>)}
+            {products.length > 0
+                ? (products.map(p => <ProductCard key={p.id} id={p.id} onClick={() => onItemClick(p.id)}/>))
+                : (<div className={'products-list__empty'}>Товаров не найдено</div>)}
         </div>
     )
 }
