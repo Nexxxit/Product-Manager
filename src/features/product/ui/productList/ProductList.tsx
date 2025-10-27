b07490aab501102d72fc62e9865b836b213cc2cf
Nexxxit <varsik.arsik@mail.ru>
2025-10-28 01:14:48 +0500

    perf: optimize productList

--- src/features/product/ui/productList/ProductList.tsx
+++ src/features/product/ui/productList/ProductList.tsx
@@ -1,6 +1,6 @@
 import {useAppSelector, useAppDispatch} from "../../../../app/store/hooks.ts";
-import {fetchProducts, selectSortedProducts, selectStatus} from "../../model";
-import {useEffect} from "react";
+import {fetchProducts, selectSortedProducts, selectStatus, selectTotalProducts} from "../../model";
+import {memo, useEffect} from "react";
 import ProductCard from "../productCard/ProductCard.tsx";
 import Spinner from "../../../../shared/ui/Spinner/Spinner.tsx";
 import './productList.css'
@@ -9,10 +9,11 @@ type ProductListProps = {
     onItemClick: (id: number) => void;
 }
 
-const ProductList = ({onItemClick}: ProductListProps) => {
+const ProductList = memo(({onItemClick}: ProductListProps) => {
     const dispatch = useAppDispatch()
     const status = useAppSelector(selectStatus)
     const products = useAppSelector(selectSortedProducts)
+    const totalProducts = useAppSelector(selectTotalProducts)
 
     useEffect(() => {
         if (status === 'idle') {
@@ -20,20 +21,26 @@ const ProductList = ({onItemClick}: ProductListProps) => {
         }
     }, [dispatch, status]);
 
-    if (status === 'loading') return (
-        <div className={'loading'}>
-            <span className={'loading__text'}>Загружаю товары...</span>
-            <Spinner/>
-        </div>
-    )
+    if (status === 'loading') {
+        return (
+            <div className={'loading'}>
+                <span className={'loading__text'}>Загружаю товары...</span>
+                <Spinner/>
+            </div>
+        )
+    }
+
+    if (totalProducts === 0) {
+        return <div className={'products-list__empty'}>Товаров не найдено</div>
+    }
 
     return (
         <div className={'products-list'}>
-            {products.length > 0
-                ? (products.map(p => <ProductCard key={p.id} id={p.id} onClick={() => onItemClick(p.id)}/>))
-                : (<div className={'products-list__empty'}>Товаров не найдено</div>)}
+            {products.map((product) => (
+                <ProductCard key={product.id} id={product.id} onItemClick={onItemClick}/>
+            ))}
         </div>
     )
-}
+})
 
 export default ProductList
